b9932965f96b2bd955c1502a00b0ccbec0389a03
TAG-Epic <admin@itzepic.net>
2020-12-26 13:30:43 +0100

    Fix: Bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="slashify",
-    version="1.0.0",
+    version="1.0.1",
     description="Automatically convert discord.py commands to slash commands!",
     author="Epic",
     author_email="tagepicuwu@gmail.com",
