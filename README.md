6471e906d04aa34d5f9de0a141121186781e3778
TAG-Epic <admin@itzepic.net>
2020-12-26 12:38:09 +0100

    Feat: Poggers lib

---
+++ README.md
@@ -0,0 +1,10 @@
+# Slashify
+Convert your discord.py commands to slash commands easily!  
+
+## Usage
+```python
+from slashify import Slashify
+...
+Slashify(bot)
+...
+```
---
+++ setup.py
@@ -0,0 +1,14 @@
+"""
+Created by Epic at 12/26/20
+"""
+from setuptools import setup, find_packages
+
+setup(
+    name="slashify",
+    version="1.0.0",
+    description="Automatically convert discord.py commands to slash commands!",
+    author="Epic",
+    author_email="tagepicuwu@gmail.com",
+    url="https://github.com/tag-epic/slashify",
+    packages=find_packages()
+)
